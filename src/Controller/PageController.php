407df0d88b27d7c66e57eeca94fa1168a3d45676
Csoda26 <charlie.clavel@lapiscine.pro>
2020-11-16 16:51:23 +0100

    response name

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,7 +1,9 @@
 <?php
 Namespace App\Controller;
 
+
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController
@@ -12,11 +14,21 @@ class PageController
     public function age(Request $request){                   // request est associé utilisé pour récupéré info
         $age = $request->query->get('age');               // soit en équivalent $GET, $POST, $COOKIES ...
         if($age>=18){
-           echo 'tes bien majeur c\'est bon';
+            echo 'tes bien majeur c\'est bon';
         }else{                                                  //si age supérieur ou égal a 18 = tu peux entrer
             echo 'Tu peux pas encore y acceder t\'es mineur';   //si age strict inf à 18 = pas entrer
         }
         die();
     }
 
+    /**
+     * @Route("/koala", name="page_poker")
+     */
+    public function recupere(Request $request){
+        $firstname =  $request->query->get('prenom');
+        $lastname =  $request->query->get('nom');
+
+        $response = new Response( '<p> Bonjour ' . $firstname . ' ' . $lastname . ' </p> ');
+            return $response;
+    }
 }
