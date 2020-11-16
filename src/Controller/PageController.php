46be44c912dc9dbf9449bc6716b000798e733a5f
Csoda26 <charlie.clavel@lapiscine.pro>
2020-11-16 15:29:40 +0100

    commentaires

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,12 +9,12 @@ class PageController
     /**
      * @Route("/poker", name="page_poker")
      */
-    public function age(Request $request){
-        $age = $request->query->get('age');
+    public function age(Request $request){                   // request est associé utilisé pour récupéré info
+        $age = $request->query->get('age');               // soit en équivalent $GET, $POST, $COOKIES ...
         if($age>=18){
-           echo 'majeur';
-        }else{
-            echo 'mineur';
+           echo 'tes bien majeur c\'est bon';
+        }else{                                                  //si age supérieur ou égal a 18 = tu peux entrer
+            echo 'Tu peux pas encore y acceder t\'es mineur';   //si age strict inf à 18 = pas entrer
         }
         die();
     }
