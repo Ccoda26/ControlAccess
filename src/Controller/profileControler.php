63ef67590cb89326c1d57569b30b16cd33e41f5b
Csoda26 <charlie.clavel@lapiscine.pro>
2020-11-18 16:47:17 +0100

    if and for

--- src/Controller/profileControler.php
+++ src/Controller/profileControler.php
@@ -24,5 +24,47 @@ class profileControler extends  AbstractController
         ]);
     }
 
+    /**
+    * @Route("/agent", name="profile_agent")
+    */
+    public function NameAgent(){
+
+        $agents = [
+            1 => [
+                "lastName" => "Robert",
+                "firstName" => "David",
+                "age" => 30,
+                "published" => true
+            ],
+            2 => [
+                "lastName" => "Labaste",
+                "firstName" => "Denis",
+                "age" => 29,
+                "published" => true
+            ],
+            3 => [
+                "lastName" => "Rozand",
+                "firstName" => "Mathieu",
+                "age" => 31,
+                "published" => false
+            ],
+            4 => [
+                "lastName" => "Despert",
+                "firstName" => "Yoann",
+                "age" => 33,
+                "published" => true
+            ],
+            5 => [
+                "lastName" => "Dorignac",
+                "firstName" => "Loic",
+                "age" => 34,
+                "published" => true
+            ]
+        ];
+
+        return $this->render('profile.html.twig', [
+            'agents' => $agents
+        ]);
+    }
 
 }
